6f233a5fe4c6a2e558b69ce07916eb0abe22bb70
Yves Huber <yves.huber@lernende.bbw.ch>
2022-09-26 11:14:36 +0000

    Updated Properties and Added Test Data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO public."USER" (
+id, "Email", "Nachname", "Passwort", "Role", "Vorname") VALUES (
+'1'::bigint, 'Test@Email.test
+'::character varying, 'Nachname'::character varying, '123456'::character varying, 'Mitglied'::character varying, 'Vorname'::character varying)
+ returning id;
+
+INSERT INTO public."BUCHUNG" (
+id, "Datum", "Halbtag", "Status", "Visablility", user_id) VALUES (
+'1'::bigint, '2022-09-26'::date, false::boolean, 'Accepted'::character varying, true::boolean, '1'::bigint)
+ returning id;
+
+ INSERT INTO public."BUCHUNG" (
+id, "Datum", "Halbtag", "Status", "Visablility", user_id) VALUES (
+'2'::bigint, '2022-09-26'::date, false::boolean, 'Accepted'::character varying, false::boolean, '1'::bigint)
+ returning id;
